81c91b70aa5a7ea2f91be3d3df8b3d77342b23e5
Nikhil Duggirala <nikhild@berkeley.edu>
2017-11-01 15:09:32 -0700

    updated cleaning functions

--- cleanDataScript.R
+++ cleanDataScript.R
@@ -1,6 +1,9 @@
 library(stringr)
+library(XML)
+library(xml2)
+library(plyr)
 parseStats <- function(inputFile) {
-  fileName <- paste("Stats/", inputFile, ".csv", sep = "")
+  fileName <- paste("RawData/Stats/", inputFile, ".csv", sep = "")
   df <- read.csv(fileName, stringsAsFactors = FALSE)
   df[1,2] <- "Name"
   names(df) <- df[1,]
@@ -25,4 +28,47 @@ parseStats <- function(inputFile) {
   playerName <- str_split(playerName, "\\*", simplify = TRUE)[,1]
   df$Name <- playerName
   return(df)
+}
+
+parseID <- function(inputFile) {
+  fileName <- paste("RawData/ID_to_Name/", inputFile, ".xml", sep="")
+  xmlData <- xmlRoot(xmlParse(fileName))
+  size <- xmlSize(xmlData)
+  id <- vector(mode="character", length=size)
+  name <- vector(mode="character", length=size)
+  position <- vector(mode="character", length=size)
+  team <- vector(mode="character", length=size)
+  for(i in 1:size) {
+    attrs <- xmlAttrs(xmlData[[i]])
+    id[i] <- attrs["id"]
+    name[i] <- attrs["name"]
+    position[i] <- attrs["position"]
+    team[i] <- attrs["team"]
+  }
+  dataFrame <- cbind(id,name,position,team)
+  dataFrame <- dataFrame[dataFrame[,"position"] == "QB" | dataFrame[,"position"] == "RB" |
+                         dataFrame[,"position"] == "WR" | dataFrame[,"position"] == "TE",]
+  return(dataFrame)
+}
+
+parseADP <- function(inputFile) {
+  fileName <- paste("RawData/ADP/", inputFile, ".xml", sep="")
+  df <- read_xml(fileName)
+  return(df)
+}
+
+writeStats <- function() {
+  for(i in 2011:2016) {
+    dataFile <- parseStats(i)
+    fileName <- paste("CleanData/Stats/", i, "_clean.csv", sep="")
+    write.csv(dataFile, fileName)
+  }
+}
+
+writeID <- function() {
+  for(i in 2011:2016) {
+    dataFile <- parseID(i)
+    fileName <- paste("CleanData/ID_to_Name/", i, "_clean.csv", sep="")
+    write.csv(dataFile, fileName)
+  }
 }
